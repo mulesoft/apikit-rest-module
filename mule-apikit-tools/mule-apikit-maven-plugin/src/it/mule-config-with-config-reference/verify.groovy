3a62f2b44cde8583e6fdcf876d53e79382e99390
juandesi <juan.desimoni@mulesoft.com>
2019-04-29 17:28:55 -0300

    APIKT-1879: separate all APIKit rest projects

--- mule-apikit-tools/mule-apikit-maven-plugin/src/it/mule-config-with-config-reference/verify.groovy
+++
@@ -1,5 +0,0 @@
-File muleConfigFile = new File( basedir, "src/main/app/leagues.xml" );
-
-assert muleConfigFile.exists()
-assert muleConfigFile.isFile()
-
--- mule-apikit-tools/mule-apikit-maven-plugin/src/it/new-mule-config/verify.groovy
+++
@@ -1,4 +0,0 @@
-File muleConfigFile = new File( basedir, "src/main/app/leagues.xml" );
-
-assert muleConfigFile.exists()
-assert muleConfigFile.isFile()
