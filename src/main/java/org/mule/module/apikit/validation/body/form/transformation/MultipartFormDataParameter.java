27cb2a2d774254ceffadb2d60ae17081b779ca1e
Juan Brasca <juan.brasca@gmail.com>
2019-05-02 16:58:52 -0300

    APIKIT-1806 Support JSON and XML files as form parameters

---
+++ src/main/java/org/mule/module/apikit/validation/body/form/transformation/MultipartFormDataParameter.java
@@ -0,0 +1,33 @@
+package org.mule.module.apikit.validation.body.form.transformation;
+
+import org.apache.commons.io.IOUtils;
+import org.mule.module.apikit.api.exception.InvalidFormParameterException;
+import org.mule.raml.interfaces.model.parameter.IParameter;
+import org.mule.runtime.api.metadata.MediaType;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class MultipartFormDataParameter {
+  private final MediaType mediaType;
+  private final InputStream inputStream;
+
+  public MultipartFormDataParameter(InputStream inputStream, MediaType mediaType){
+    this.inputStream = inputStream;
+    this.mediaType = mediaType;
+  }
+
+  public void validate(IParameter parameter) throws InvalidFormParameterException {
+    if(mediaType.matches(MediaType.TEXT) ){
+      String value;
+      try {
+        value = IOUtils.toString(inputStream);
+      } catch (IOException e) {
+        throw new InvalidFormParameterException(e);
+      }
+      if(!parameter.validate(value))
+        throw new InvalidFormParameterException("Value " + value + " for parameter " + parameter.getDisplayName() + " is invalid");
+    }
+  }
+
+}
