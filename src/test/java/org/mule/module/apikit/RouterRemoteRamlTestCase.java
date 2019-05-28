db3a25e39cd5c0a6270a442cda62d65c950f4ff5
Juan Desimoni <juan.desimoni@mulesoft.com>
2019-05-28 19:10:24 -0300

    Fix test pointing to invalid raml URL (#17)

--- src/test/java/org/mule/module/apikit/RouterRemoteRamlTestCase.java
+++ src/test/java/org/mule/module/apikit/RouterRemoteRamlTestCase.java
@@ -6,20 +6,56 @@
  */
 package org.mule.module.apikit;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.mockserver.integration.ClientAndServer;
+import org.mockserver.model.HttpResponse;
 
 import static com.jayway.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
+import static org.mockserver.matchers.Times.unlimited;
+import static org.mockserver.model.HttpRequest.request;
+
+import org.mule.runtime.core.api.util.IOUtils;
+import org.mule.tck.junit4.rule.DynamicPort;
+import org.mule.tck.junit4.rule.SystemProperty;
+
+import java.io.InputStream;
 
 public class RouterRemoteRamlTestCase extends AbstractMultiParserFunctionalTestCase {
+
+  @Rule
+  public DynamicPort port = new DynamicPort("http.server.port");
+
+  private ClientAndServer server;
 
   @Override
   protected String getConfigFile() {
     return "org/mule/module/apikit/router-remote-raml/remote-raml.xml";
   }
 
+  @Override
+  protected void doSetUpBeforeMuleContextCreation() {
+    this.server = ClientAndServer.startClientAndServer(port.getNumber());
+    this.server
+      .when(request().withMethod("GET").withPath("/testRaml"), unlimited())
+      .respond(HttpResponse.response().withBody(getTestRamlAsString())
+                  .withStatusCode(200)
+                  .withHeader("Content-Type", "application/raml+yaml"));
+    super.doSetUpBeforeMuleContextCreation();
+  }
+
+  private String getTestRamlAsString() {
+    try {
+      ClassLoader ccl = Thread.currentThread().getContextClassLoader();
+      return IOUtils.toString(ccl.getResourceAsStream("org/mule/module/apikit/router-remote-raml/remote.raml"));
+    } catch (Exception e) {
+      throw  new RuntimeException("fail to create HTTP Test Server", e);
+    }
+  }
+
   @Test
-  public void simpleRouting() throws Exception {
+  public void simpleRouting() {
     given().header("Accept", "*/*")
         .expect()
         .response().body(is("hello"))
