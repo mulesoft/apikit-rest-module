2f61b07dd42901065aa731d8dcc2b027bec00535
JuanAller <juanmariaaller@gmail.com>
2019-08-28 13:18:20 -0300

    APIKIT-1982: APIKit validation stopping after first error (#62)

---
+++ src/test/java/org/mule/module/apikit/validation/body/schema/RestSchemaV2ValidatorTestCase.java
@@ -0,0 +1,40 @@
+package org.mule.module.apikit.validation.body.schema;
+
+import org.junit.Test;
+import org.junit.Rule;
+import org.junit.rules.ExpectedException;
+import org.mule.apikit.validation.ApiValidationResult;
+import org.mule.module.apikit.validation.body.schema.v2.RestSchemaV2Validator;
+import org.mule.apikit.model.MimeType;
+import org.mule.module.apikit.api.exception.BadRequestException;
+
+import static java.util.Arrays.*;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class RestSchemaV2ValidatorTestCase {
+
+  @Rule
+  public ExpectedException expected = ExpectedException.none();
+
+  @Test
+  public void buildLogMessage() throws Exception {
+    expected.expect(BadRequestException.class);
+    expected.expectMessage("Message 0\nMessage 1");
+
+    MimeType mimeTypeMock = mock(MimeType.class);
+    RestSchemaV2Validator validator = new RestSchemaV2Validator(mimeTypeMock);
+
+    ApiValidationResult validationResult0 = mock(ApiValidationResult.class);
+    when(validationResult0.getMessage()).thenReturn("Message 0");
+    ApiValidationResult validationResult1 = mock(ApiValidationResult.class);
+    when(validationResult1.getMessage()).thenReturn("Message 1");
+
+    when(mimeTypeMock.validate(any(String.class))).thenReturn(
+        asList(validationResult0, validationResult1));
+
+    validator.validate("{\"test\" : \"test\"}");
+  }
+
+}
