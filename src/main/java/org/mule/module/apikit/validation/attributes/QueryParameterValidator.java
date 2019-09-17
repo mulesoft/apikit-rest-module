1dabc1e1d035e40a1e8999e475261244507b0b49
Juan Brasca <juan.brasca@gmail.com>
2019-09-17 17:07:14 -0300

    APIKIT-2008: Support string query param starting with asterisk

--- src/main/java/org/mule/module/apikit/validation/attributes/QueryParameterValidator.java
+++ src/main/java/org/mule/module/apikit/validation/attributes/QueryParameterValidator.java
@@ -103,16 +103,19 @@ public class QueryParameterValidator {
       builder.append("\n");
     });
 
-    validateQueryParam(paramKey, expected, builder.toString());
+    validateQueryParam(paramKey, expected, builder.toString(),false);
   }
 
   private void validateQueryParam(String paramKey, Parameter expected, String paramValue) throws InvalidQueryParameterException {
-    if (!expected.validate(paramValue)) {
-      String msg = String.format("\"Invalid value '%s' for query parameter %s. %s\"",
-                                 paramValue, paramKey, expected.message(paramValue));
+    validateQueryParam(paramKey,expected,paramValue,true);
+  }
 
+  private void validateQueryParam(String paramKey, Parameter expected, String paramValue, boolean sanitize) throws InvalidQueryParameterException {
+    String sanitizedParamValue = sanitize ? expected.surroundWithQuotesIfNeeded(paramValue) : paramValue;
+    if (!expected.validate(sanitizedParamValue)) {
+      String msg = String.format("\"Invalid value '%s' for query parameter %s. %s\"",
+              paramValue, paramKey, expected.message(paramValue));
       throw new InvalidQueryParameterException(msg);
     }
   }
-
 }
