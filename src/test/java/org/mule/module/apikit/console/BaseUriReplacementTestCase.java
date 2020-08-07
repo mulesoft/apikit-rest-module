51b13b5bf35e1ecb6a01dc71cb6edcbace85862f
JuanAller <juan.aller@mulesoft.com>
2020-08-06 21:29:12 -0300

    APIKIT-2708: console always point to localhost (#244) (#245)

--- src/test/java/org/mule/module/apikit/console/BaseUriReplacementTestCase.java
+++ src/test/java/org/mule/module/apikit/console/BaseUriReplacementTestCase.java
@@ -6,18 +6,22 @@
  */
 package org.mule.module.apikit.console;
 
-import org.junit.AfterClass;
+import org.junit.After;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.mule.module.apikit.api.RamlHandler;
 import org.mule.module.apikit.api.UrlUtils;
-import org.mule.parser.service.ParserMode;
 import org.mule.runtime.api.exception.ErrorTypeRepository;
 import org.mule.runtime.core.api.MuleContext;
 
+import static java.lang.System.clearProperty;
+import static java.lang.System.setProperty;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
+import static org.mule.module.apikit.api.UrlUtils.getBaseUriReplacement;
+import static org.mule.module.apikit.api.UrlUtils.replaceHostInURL;
 import static org.mule.parser.service.ParserMode.AUTO;
 
 public class BaseUriReplacementTestCase {
@@ -39,111 +43,139 @@ public class BaseUriReplacementTestCase {
     assertEquals("http://localhost:8081/api", ramlHandler.getBaseUriReplacement("http://localhost:8081/api"));
     assertEquals("http://localhost:8081/api", ramlHandler.getBaseUriReplacement("http://0.0.0.0:8081/api"));
 
-    System.setProperty(FULL_DOMAIN, "pepe.cloudhub.io");
+    setProperty(FULL_DOMAIN, "pepe.cloudhub.io");
     assertEquals("http://localhost:8081/api", ramlHandler.getBaseUriReplacement("http://localhost:8081/api"));
     assertEquals("http://pepe.cloudhub.io/api", ramlHandler.getBaseUriReplacement("http://0.0.0.0:8081/api"));
 
-    System.setProperty(FULL_DOMAIN, "http://pepe.cloudhub.io");
+    setProperty(FULL_DOMAIN, "http://pepe.cloudhub.io");
     assertEquals("http://localhost:8081/api", ramlHandler.getBaseUriReplacement("http://localhost:8081/api"));
     assertEquals("http://pepe.cloudhub.io/api", ramlHandler.getBaseUriReplacement("http://0.0.0.0:8081/api"));
 
-    System.setProperty(FULL_DOMAIN, "http://pepe.cloudhub.io/");
+    setProperty(FULL_DOMAIN, "http://pepe.cloudhub.io/");
     assertEquals("http://localhost:8081/api", ramlHandler.getBaseUriReplacement("http://localhost:8081/api"));
     assertEquals("http://pepe.cloudhub.io/api", ramlHandler.getBaseUriReplacement("http://0.0.0.0:8081/api"));
 
-    System.setProperty(FULL_DOMAIN, "pepe.cloudhub.io/");
+    setProperty(FULL_DOMAIN, "pepe.cloudhub.io/");
     assertEquals("http://localhost:8081/api", ramlHandler.getBaseUriReplacement("http://localhost:8081/api"));
     assertEquals("http://pepe.cloudhub.io/api", ramlHandler.getBaseUriReplacement("http://0.0.0.0:8081/api"));
 
-    System.setProperty(FULL_DOMAIN, "pepe.cloudhub.io");
+    setProperty(FULL_DOMAIN, "pepe.cloudhub.io");
     assertEquals("http://localhost:8081/api/", ramlHandler.getBaseUriReplacement("http://localhost:8081/api/"));
     assertEquals("http://pepe.cloudhub.io/api/", ramlHandler.getBaseUriReplacement("http://0.0.0.0:8081/api/"));
 
-    System.setProperty(FULL_DOMAIN, "http://pepe.cloudhub.io");
+    setProperty(FULL_DOMAIN, "http://pepe.cloudhub.io");
     assertEquals("http://pepe.cloudhub.io/api/", ramlHandler.getBaseUriReplacement("http://0.0.0.0:8081/api/"));
 
-    System.setProperty(FULL_DOMAIN, "http://pepe.cloudhub.io/");
+    setProperty(FULL_DOMAIN, "http://pepe.cloudhub.io/");
     assertEquals("http://pepe.cloudhub.io/api/", ramlHandler.getBaseUriReplacement("http://0.0.0.0:8081/api/"));
 
-    System.setProperty(FULL_DOMAIN, "pepe.cloudhub.io/");
+    setProperty(FULL_DOMAIN, "pepe.cloudhub.io/");
     assertEquals("http://pepe.cloudhub.io/api/", ramlHandler.getBaseUriReplacement("http://0.0.0.0:8081/api/"));
 
-    System.setProperty(FULL_DOMAIN, "pepe.cloudhub.io");
+    setProperty(FULL_DOMAIN, "pepe.cloudhub.io");
     assertEquals("http://localhost:8081/", ramlHandler.getBaseUriReplacement("http://localhost:8081/"));
     assertEquals("http://pepe.cloudhub.io/", ramlHandler.getBaseUriReplacement("http://0.0.0.0:8081/"));
 
-    System.setProperty(FULL_DOMAIN, "http://pepe.cloudhub.io");
+    setProperty(FULL_DOMAIN, "http://pepe.cloudhub.io");
     assertEquals("http://pepe.cloudhub.io/", ramlHandler.getBaseUriReplacement("http://0.0.0.0:8081/"));
 
-    System.setProperty(FULL_DOMAIN, "http://pepe.cloudhub.io/");
+    setProperty(FULL_DOMAIN, "http://pepe.cloudhub.io/");
     assertEquals("http://pepe.cloudhub.io/", ramlHandler.getBaseUriReplacement("http://0.0.0.0:8081/"));
 
-    System.setProperty(FULL_DOMAIN, "pepe.cloudhub.io/");
+    setProperty(FULL_DOMAIN, "pepe.cloudhub.io/");
     assertEquals("http://pepe.cloudhub.io/", ramlHandler.getBaseUriReplacement("http://0.0.0.0:8081/"));
 
-    System.setProperty(FULL_DOMAIN, "pepe.cloudhub.io");
+    setProperty(FULL_DOMAIN, "pepe.cloudhub.io");
     assertEquals("http://localhost:8081", ramlHandler.getBaseUriReplacement("http://localhost:8081"));
     assertEquals("http://pepe.cloudhub.io", ramlHandler.getBaseUriReplacement("http://0.0.0.0:8081"));
 
-    System.setProperty(FULL_DOMAIN, "http://pepe.cloudhub.io");
+    setProperty(FULL_DOMAIN, "http://pepe.cloudhub.io");
     assertEquals("http://pepe.cloudhub.io", ramlHandler.getBaseUriReplacement("http://0.0.0.0:8081"));
 
-    System.setProperty(FULL_DOMAIN, "http://pepe.cloudhub.io/");
+    setProperty(FULL_DOMAIN, "http://pepe.cloudhub.io/");
     assertEquals("http://pepe.cloudhub.io/", ramlHandler.getBaseUriReplacement("http://0.0.0.0:8081"));
 
-    System.setProperty(FULL_DOMAIN, "pepe.cloudhub.io/");
+    setProperty(FULL_DOMAIN, "pepe.cloudhub.io/");
     assertEquals("http://pepe.cloudhub.io/", ramlHandler.getBaseUriReplacement("http://0.0.0.0:8081"));
 
-    System.setProperty(FULL_DOMAIN, "pepe.cloudhub.io/api");
+    setProperty(FULL_DOMAIN, "pepe.cloudhub.io/api");
     assertEquals("http://pepe.cloudhub.io/api", ramlHandler.getBaseUriReplacement("http://0.0.0.0:8081"));
 
-    System.setProperty(FULL_DOMAIN, "http://pepe.cloudhub.io/api");
+    setProperty(FULL_DOMAIN, "http://pepe.cloudhub.io/api");
     assertEquals("http://pepe.cloudhub.io/api", ramlHandler.getBaseUriReplacement("http://0.0.0.0:8081"));
 
-    System.setProperty(FULL_DOMAIN, "http://pepe.cloudhub.io/api");
+    setProperty(FULL_DOMAIN, "http://pepe.cloudhub.io/api");
     assertEquals("http://pepe.cloudhub.io/api", ramlHandler.getBaseUriReplacement("http://0.0.0.0:8081"));
 
-    System.setProperty(FULL_DOMAIN, "pepe.cloudhub.io/api");
+    setProperty(FULL_DOMAIN, "pepe.cloudhub.io/api");
     assertEquals("http://pepe.cloudhub.io/api", ramlHandler.getBaseUriReplacement("http://0.0.0.0:8081"));
   }
 
   @Test
-  public void consoleUriReplacementTest() throws Exception {
-    // Console Replacements
-    System.clearProperty(FULL_DOMAIN);
-    assertEquals("http://localhost:8081/console", UrlUtils.getBaseUriReplacement("http://localhost:8081/console"));
-    assertEquals("http://localhost:8081/console/", UrlUtils.getBaseUriReplacement("http://localhost:8081/console/"));
+  public void consoleUriReplacementTest() {
+    assertEquals("http://localhost:8081/console", getBaseUriReplacement("http://localhost:8081/console"));
+    assertEquals("http://localhost:8081/console/", getBaseUriReplacement("http://localhost:8081/console/"));
 
-    System.setProperty(FULL_DOMAIN, "http://aamura.cloudhub.io/api");
-    assertEquals("http://aamura.cloudhub.io/api/console", UrlUtils.getBaseUriReplacement("http://0.0.0.0:8081/console"));
-    assertEquals("http://aamura.cloudhub.io/api/console/", UrlUtils.getBaseUriReplacement("http://0.0.0.0:8081/console/"));
+    setProperty(FULL_DOMAIN, "http://aamura.cloudhub.io/api");
+    assertEquals("http://aamura.cloudhub.io/api/console", getBaseUriReplacement("http://0.0.0.0:8081/console"));
+    assertEquals("http://aamura.cloudhub.io/api/console/", getBaseUriReplacement("http://0.0.0.0:8081/console/"));
 
-    System.setProperty(FULL_DOMAIN, "http://aamura.cloudhub.io/api/");
-    assertEquals("http://aamura.cloudhub.io/api/console", UrlUtils.getBaseUriReplacement("http://0.0.0.0:8081/console"));
-    assertEquals("http://aamura.cloudhub.io/api/console/", UrlUtils.getBaseUriReplacement("http://0.0.0.0:8081/console/"));
+    setProperty(FULL_DOMAIN, "http://aamura.cloudhub.io/api/");
+    assertEquals("http://aamura.cloudhub.io/api/console", getBaseUriReplacement("http://0.0.0.0:8081/console"));
+    assertEquals("http://aamura.cloudhub.io/api/console/", getBaseUriReplacement("http://0.0.0.0:8081/console/"));
 
-    System.setProperty(FULL_DOMAIN, "https://aamura.cloudhub.io/api");
-    assertEquals("https://aamura.cloudhub.io/api/console", UrlUtils.getBaseUriReplacement("http://0.0.0.0:8081/console"));
-    assertEquals("https://aamura.cloudhub.io/api/console/", UrlUtils.getBaseUriReplacement("http://0.0.0.0:8081/console/"));
+    setProperty(FULL_DOMAIN, "https://aamura.cloudhub.io/api");
+    assertEquals("https://aamura.cloudhub.io/api/console", getBaseUriReplacement("http://0.0.0.0:8081/console"));
+    assertEquals("https://aamura.cloudhub.io/api/console/", getBaseUriReplacement("http://0.0.0.0:8081/console/"));
 
-    System.setProperty(FULL_DOMAIN, "https://aamura.cloudhub.io/api/");
-    assertEquals("https://aamura.cloudhub.io/api/console", UrlUtils.getBaseUriReplacement("http://0.0.0.0:8081/console"));
-    assertEquals("https://aamura.cloudhub.io/api/console/", UrlUtils.getBaseUriReplacement("http://0.0.0.0:8081/console/"));
+    setProperty(FULL_DOMAIN, "https://aamura.cloudhub.io/api/");
+    assertEquals("https://aamura.cloudhub.io/api/console", getBaseUriReplacement("http://0.0.0.0:8081/console"));
+    assertEquals("https://aamura.cloudhub.io/api/console/", getBaseUriReplacement("http://0.0.0.0:8081/console/"));
 
-    System.setProperty(FULL_DOMAIN, "https://aamura.cloudhub.io/api/v1");
-    assertEquals("https://aamura.cloudhub.io/api/v1/console", UrlUtils.getBaseUriReplacement("http://0.0.0.0:8081/console"));
-    assertEquals("https://aamura.cloudhub.io/api/v1/console/", UrlUtils.getBaseUriReplacement("http://0.0.0.0:8081/console/"));
+    setProperty(FULL_DOMAIN, "https://aamura.cloudhub.io/api/v1");
+    assertEquals("https://aamura.cloudhub.io/api/v1/console", getBaseUriReplacement("http://0.0.0.0:8081/console"));
+    assertEquals("https://aamura.cloudhub.io/api/v1/console/", getBaseUriReplacement("http://0.0.0.0:8081/console/"));
 
-    System.setProperty(FULL_DOMAIN, "aamura.cloudhub.io/api/v1");
-    assertEquals("https://aamura.cloudhub.io/api/v1/console", UrlUtils.getBaseUriReplacement("https://0.0.0.0:8081/console"));
-    assertEquals("https://aamura.cloudhub.io/api/v1/console/", UrlUtils.getBaseUriReplacement("https://0.0.0.0:8081/console/"));
+    setProperty(FULL_DOMAIN, "aamura.cloudhub.io/api/v1");
+    assertEquals("https://aamura.cloudhub.io/api/v1/console", getBaseUriReplacement("https://0.0.0.0:8081/console"));
+    assertEquals("https://aamura.cloudhub.io/api/v1/console/", getBaseUriReplacement("https://0.0.0.0:8081/console/"));
 
-    assertEquals("http://aamura.cloudhub.io/api/v1/console", UrlUtils.getBaseUriReplacement("http://0.0.0.0:8081/console"));
-    assertEquals("http://aamura.cloudhub.io/api/v1/console/", UrlUtils.getBaseUriReplacement("http://0.0.0.0:8081/console/"));
+    assertEquals("http://aamura.cloudhub.io/api/v1/console", getBaseUriReplacement("http://0.0.0.0:8081/console"));
+    assertEquals("http://aamura.cloudhub.io/api/v1/console/", getBaseUriReplacement("http://0.0.0.0:8081/console/"));
   }
 
-  @AfterClass
-  public static void after() {
-    System.clearProperty(FULL_DOMAIN);
+  @Test
+  public void withoutSystemProperty() {
+    assertEquals("http://localhost:48518/api/", getBaseUriReplacement("http://0.0.0.0:48518/api/"));
+    assertEquals("http://localhost:48518/api/", getBaseUriReplacement("http://localhost:48518/api/"));
+    assertNull(getBaseUriReplacement(null));
+
   }
+
+  @Test
+  public void replaceHostWithIncomingRequestHost() {
+    assertEquals("http://localhost:48518/api", replaceHostInURL("http://0.0.0.0:48518/api", "http://localhost:48518"));
+    assertEquals("http://localhost:48518/api/", replaceHostInURL("http://0.0.0.0:48518/api/", "http://localhost:48518"));
+    assertEquals("http://localhost:48518/api/", replaceHostInURL("http://0.0.0.0:48518/api/", "localhost:48518"));
+    assertEquals("https://127.0.0.1:48518/api/", replaceHostInURL("https://0.0.0.0:48518/api/", "https://127.0.0.1:48518"));
+    assertEquals("http://192.168.0.196:48518/api/", replaceHostInURL("http://0.0.0.0:48518/api/", "http://192.168.0.196:48518"));
+    assertEquals("https://192.168.0.196:48518/api/",
+                 replaceHostInURL("https://0.0.0.0:48518/api/", "https://192.168.0.196:48518"));
+    assertEquals("https://192.168.0.196:48518/api/", replaceHostInURL("https://0.0.0.0:48518/api/", "192.168.0.196:48518"));
+  }
+
+  @Test
+  public void replaceHostWithSystemProperty() {
+    // using host from system property, instead from incoming request
+    setProperty(FULL_DOMAIN, "aamura.cloudhub.io/v1");
+    assertEquals("https://aamura.cloudhub.io/v1/api/", replaceHostInURL("https://0.0.0.0:48518/api/", "192.168.0.196:48518"));
+    setProperty(FULL_DOMAIN, "https://aamura.cloudhub.io/v1");
+    assertEquals("https://aamura.cloudhub.io/v1/api/", replaceHostInURL("https://0.0.0.0:48518/api/", "192.168.0.196:48518"));
+  }
+
+  @After
+  public void after() {
+    clearProperty(FULL_DOMAIN);
+  }
+
 }
