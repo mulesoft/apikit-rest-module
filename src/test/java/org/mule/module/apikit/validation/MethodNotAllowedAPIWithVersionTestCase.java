2c9324c30fc873cb030b3c0722476fbcfbe7eef5
Damián Courtil <dcourtil@mulesoft.com>
2020-09-11 16:06:37 -0300

    APIKIT-2750: BaseUriParameters overridden in a method are also returned for sibling methods (#278)

--- src/test/java/org/mule/module/apikit/validation/MethodNotAllowedAPIWithVersionTestCase.java
+++ src/test/java/org/mule/module/apikit/validation/MethodNotAllowedAPIWithVersionTestCase.java
@@ -6,48 +6,24 @@
  */
 package org.mule.module.apikit.validation;
 
-import static org.hamcrest.core.IsEqual.equalTo;
-
-import java.io.InputStream;
-import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.ExpectedException;
 import org.mule.module.apikit.api.exception.MethodNotAllowedException;
 import org.mule.module.apikit.api.exception.MuleRestException;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+
 public class MethodNotAllowedAPIWithVersionTestCase extends AbstractRequestValidatorTestCase {
-
-  @Rule
-  public ExpectedException expectedException = ExpectedException.none();
-
-  @Override
-  protected String getPath() {
-    return "/api/path/1.0/job/123";
-  }
-
-  protected String getRelativePath() {
-    return "/path/1.0/job/123";
-  }
-
-  @Override
-  protected String getMethod() {
-    return "POST";
-  }
-
-  @Override
-  protected String getApiLocation() {
-    return "unit/validation/api-with-version.raml";
-  }
-
-  @Override
-  protected InputStream getBody() {
-    return null;
-  }
 
   @Test
   public void throwMethodNotAllowedException() throws MuleRestException {
     expectedException.expect(MethodNotAllowedException.class);
     expectedException.expectMessage(equalTo("HTTP Method post not allowed for : /path/{version}/job/{jobId}"));
-    super.validateRequest();
+    testRestRequestValidatorBuilder
+        .withApiLocation("unit/validation/api-with-version.raml")
+        .withRequestPath("/api/path/1.0/job/123")
+        .withRelativePath("/path/1.0/job/123")
+        .withMethod("POST")
+        .build()
+        .validateRequest();
   }
 }
