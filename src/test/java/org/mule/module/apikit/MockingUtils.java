95a84ffd4cbc6b9c315f0cf13083ad510ea49682
Damián Courtil <dcourtil@mulesoft.com>
2020-10-15 12:46:01 -0300

    APIKIT-2794: Invalid flow routing strategy for new versioning schema (#313) (#316)

---
+++ src/test/java/org/mule/module/apikit/MockingUtils.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.module.apikit;
+
+import java.lang.reflect.Field;
+
+public class MockingUtils {
+
+  public static void setAccessible(Field field, Object newValue) throws Exception {
+    field.setAccessible(true);
+    Field modifiersField = Field.class.getDeclaredField("modifiers");
+    modifiersField.setAccessible(true);
+    field.set(null, newValue);
+  }
+
+}
