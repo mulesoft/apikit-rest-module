70d8ba076f6eaffd8d107d0d257687e467bd46c2
Damián Courtil <dcourtil@mulesoft.com>
2020-12-16 13:50:46 -0300

    APIKIT-2826: APIKit not validating 'Accept' header correctly and returning an appropriate http code (#353) (#358)
    * APIKIT-2826: APIKit not validating 'Accept' header correctly and returning an appropriate http code (#353)
    
    * APIKIT-2750: BaseUriParameters overridden in a method are also returned for sibling methods (#278)
    
    * APIKIT-2826: Update Unit Tests

---
+++ src/test/java/org/mule/module/apikit/validation/HeaderParamsRequestValidator.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.module.apikit.validation;
+
+import org.junit.Test;
+import org.mule.module.apikit.api.exception.MuleRestException;
+import org.mule.module.apikit.exception.NotAcceptableException;
+import org.mule.runtime.api.util.MultiMap;
+
+import java.io.InputStream;
+import java.nio.charset.Charset;
+
+import static org.apache.commons.io.IOUtils.toInputStream;
+import static org.mule.module.apikit.helpers.PayloadHelper.makePayloadRepeatable;
+
+public class HeaderParamsRequestValidator extends AbstractRequestValidatorTestCase {
+
+  private void validateRequestForAcceptHeader(String acceptHeaderValue) throws MuleRestException {
+    MultiMap<String, String> headers = new MultiMap<>();
+    headers.put("Content-Type", "application/json");
+    headers.put("Accept", acceptHeaderValue);
+    testRestRequestValidatorBuilder
+        .withApiLocation("unit/validation/mime-types-api.raml")
+        .withRequestPath("/api/testMimeTypes")
+        .withRelativePath("/testMimeTypes")
+        .withMethod("POST")
+        .withHeaders(headers)
+        .withBody((InputStream) makePayloadRepeatable(toInputStream("{\"message\":\"All Ok\"}", Charset.defaultCharset())))
+        .build()
+        .validateRequest();
+  }
+
+  @Test
+  public void nullSubTypeThrowsNotAcceptableException() throws MuleRestException {
+    expectedException.expect(NotAcceptableException.class);
+    validateRequestForAcceptHeader("application/");
+  }
+
+  @Test
+  public void nullSubTypeWoSlashThrowsNotAcceptableException() throws MuleRestException {
+    expectedException.expect(NotAcceptableException.class);
+    validateRequestForAcceptHeader("application");
+  }
+
+  @Test
+  public void nullTypeAndSubTypeThrowsNotAcceptableException() throws MuleRestException {
+    expectedException.expect(NotAcceptableException.class);
+    validateRequestForAcceptHeader("/");
+  }
+
+  @Test
+  public void nullTypeThrowsNotAcceptableException() throws MuleRestException {
+    expectedException.expect(NotAcceptableException.class);
+    validateRequestForAcceptHeader("/json");
+  }
+
+  @Test
+  public void invalidTypeThrowsNotAcceptableException() throws MuleRestException {
+    expectedException.expect(NotAcceptableException.class);
+    validateRequestForAcceptHeader("application/xml");
+  }
+
+  @Test
+  public void successWithValidAcceptHeaderValue() throws MuleRestException {
+    validateRequestForAcceptHeader("application/json");
+  }
+}
