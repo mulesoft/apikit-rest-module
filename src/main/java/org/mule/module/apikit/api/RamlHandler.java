73420f8d92cd7b0bb6179835a1efa4af1dd14547
Damián Courtil <dcourtil@mulesoft.com>
2021-01-28 16:52:36 -0300

    APIKIT-2856: Improve validation performance (#389)
    * APIKIT-2847: Improve validation performance (#380)
    
    * APIKIT-2856: Improve validation performance (#386)

--- src/main/java/org/mule/module/apikit/api/RamlHandler.java
+++ src/main/java/org/mule/module/apikit/api/RamlHandler.java
@@ -30,7 +30,6 @@ import org.slf4j.LoggerFactory;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URL;
