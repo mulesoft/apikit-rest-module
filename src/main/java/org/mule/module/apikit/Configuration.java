6dd65618de015175329a9988b7808337ebbb7c78
Rodrigo Merino <elrodro83@users.noreply.github.com>
2021-07-19 17:26:45 -0300

    MULE-19490: Make extension model consistent with the parsers definition (#428)

--- src/main/java/org/mule/module/apikit/Configuration.java
+++ src/main/java/org/mule/module/apikit/Configuration.java
@@ -6,11 +6,9 @@
  */
 package org.mule.module.apikit;
 
-import com.github.fge.jsonschema.main.JsonSchema;
-import com.google.common.cache.CacheBuilder;
-import com.google.common.cache.CacheLoader;
-import com.google.common.cache.LoadingCache;
-import org.apache.commons.lang.StringUtils;
+import static java.lang.Runtime.getRuntime;
+import static org.mule.module.apikit.ApikitErrorTypes.errorRepositoryFrom;
+
 import org.mule.apikit.ApiType;
 import org.mule.module.apikit.api.RamlHandler;
 import org.mule.module.apikit.api.config.ConsoleConfig;
@@ -32,19 +30,26 @@ import org.mule.runtime.api.scheduler.SchedulerConfig;
 import org.mule.runtime.api.scheduler.SchedulerService;
 import org.mule.runtime.core.api.MuleContext;
 import org.mule.runtime.core.api.el.ExpressionManager;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-import javax.inject.Inject;
-import javax.xml.validation.Schema;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Optional;
 import java.util.ServiceLoader;
 import java.util.concurrent.ExecutionException;
 
-import static java.lang.Runtime.getRuntime;
-import static org.mule.module.apikit.ApikitErrorTypes.errorRepositoryFrom;
+import javax.inject.Inject;
+import javax.xml.validation.Schema;
+
+import org.apache.commons.lang.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.github.fge.jsonschema.main.JsonSchema;
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
 
 public class Configuration implements Disposable, Initialisable, ValidationConfig, ConsoleConfig {
 
@@ -70,7 +75,7 @@ public class Configuration implements Disposable, Initialisable, ValidationConfi
   private boolean keepRamlBaseUri;
   private String outboundHeadersMapName;
   private String httpStatusVarName;
-  private FlowMappings flowMappings = new FlowMappings();
+  private List<FlowMapping> flowMappings = new ArrayList<>();
 
   private LoadingCache<String, JsonSchema> jsonSchemaCache;
   private LoadingCache<String, Schema> xmlSchemaCache;
@@ -112,7 +117,7 @@ public class Configuration implements Disposable, Initialisable, ValidationConfi
     } catch (Exception e) {
       throw new InitialisationException(e.fillInStackTrace(), this);
     }
-    flowFinder = new FlowFinder(ramlHandler, getName(), locator, flowMappings.getFlowMappings(),
+    flowFinder = new FlowFinder(ramlHandler, getName(), locator, flowMappings,
                                 errorRepositoryFrom(muleContext));
     buildResourcePatternCaches();
     registry.registerConfiguration(this);
@@ -206,11 +211,11 @@ public class Configuration implements Disposable, Initialisable, ValidationConfi
     this.keepApiBaseUri = keepApiBaseUri;
   }
 
-  public FlowMappings getFlowMappings() {
+  public List<FlowMapping> getFlowMappings() {
     return flowMappings;
   }
 
-  public void setFlowMappings(FlowMappings flowMappings) {
+  public void setFlowMappings(List<FlowMapping> flowMappings) {
     this.flowMappings = flowMappings;
   }
 
