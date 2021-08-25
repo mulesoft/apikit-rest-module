6a87ff507ca60a99972b1a4b6bd8202858201c1b
Damián Courtil <dcourtil@mulesoft.com>
2021-08-25 16:26:14 -0300

    APIKIT-3026: Validate query params and query string consistently (#441) (#443)

---
+++ src/main/java/org/mule/module/apikit/validation/attributes/ValidationUtils.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.module.apikit.validation.attributes;
+
+import org.mule.apikit.model.parameter.Parameter;
+
+public class ValidationUtils {
+
+  static String escapeAndSurroundWithQuotesIfNeeded(Parameter facet, String value) {
+    return facet != null && (facet.isScalar() || (facet.isArray() && !value.startsWith("{")))
+        ? facet.surroundWithQuotesIfNeeded(value.replace("\"", "\\\""))
+        : value;
+  }
+
+}
