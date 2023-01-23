caf5fea91ded66dc52da8bbe6a6b1c903899f732
Damián Courtil <dcourtil@mulesoft.com>
2023-01-23 15:02:45 -0300

    W-11365512: Fix parsing of repeated array headers with quotes (#630)

--- src/main/java/org/mule/module/apikit/deserializing/ArrayHeaderAttributeDeserializer.java
+++ src/main/java/org/mule/module/apikit/deserializing/ArrayHeaderAttributeDeserializer.java
@@ -6,24 +6,20 @@
  */
 package org.mule.module.apikit.deserializing;
 
+import com.google.common.collect.ImmutableSet;
 import org.mule.module.apikit.api.deserializing.ArrayHeaderDelimiter;
 
 import java.util.ArrayList;
 import java.util.List;
-
-import static java.util.Collections.emptyList;
-import static org.apache.commons.lang3.StringUtils.isBlank;
-import static org.apache.commons.lang3.StringUtils.isNotBlank;
+import java.util.Set;
+import java.util.Stack;
 
 /**
  * Deserializer for array header attributes.
  */
 public class ArrayHeaderAttributeDeserializer extends BaseAttributeDeserializer {
 
-  private static final char DOUBLE_QUOTES = '"';
-  private static final char OPENING_CURLY_BRACE = '{';
-  private static final char CLOSING_CURLY_BRACE = '}';
-
+  private static final Set<Character> SPACE_CHARS = ImmutableSet.of(' ', '\n', '\t', '\r', '\f');
   private ArrayHeaderDelimiter arrayHeaderDelimiter;
 
   public ArrayHeaderAttributeDeserializer(ArrayHeaderDelimiter arrayHeaderDelimiter) {
@@ -32,74 +28,137 @@ public class ArrayHeaderAttributeDeserializer extends BaseAttributeDeserializer 
 
   @Override
   public List<String> deserializeValue(String attributeValue) {
-    if (isBlank(attributeValue)) {
-      return emptyList();
-    }
     final char delimiter = arrayHeaderDelimiter.getDelimiterValue().charAt(0);
     char[] chars = attributeValue.toCharArray();
-    StringBuffer curVal = new StringBuffer();
-    boolean inQuotes = false;
-    int curlyBracesStackCount = 0;
-    boolean startCollectChar = false;
-
-    List<String> headerValues = new ArrayList<>();
-    for (char ch : chars) {
-      if (inQuotes) {
-        startCollectChar = true;
-        if (ch == DOUBLE_QUOTES) {
-          inQuotes = curlyBracesStackCount > 0 ? true : false;
-          if (inQuotes) {
-            curVal.append(ch);
-          }
-        } else if (ch == OPENING_CURLY_BRACE) {
-          curlyBracesStackCount++;
-          curVal.append(ch);
-        } else if (ch == CLOSING_CURLY_BRACE) {
-          curlyBracesStackCount--;
-          curVal.append(ch);
-        } else {
-          curVal.append(ch);
+    List<String> values = new ArrayList<>();
+    int current = 0;
+    while (current < chars.length) {
+      current = skipWhiteSpace(current, chars);
+      if (current == chars.length) {
+        // Blank was found
+        values.add("");
+      } else if (chars[current] == delimiter) {
+        // Delimiter was found, considering left side as a blank value
+        values.add("");
+        current++;
+        if (current == chars.length) {
+          // End of string reached after a delimiter was found, considering right side as a blank value
+          values.add("");
         }
-      } else if (curlyBracesStackCount > 0) {
-        if (ch == CLOSING_CURLY_BRACE) {
-          curlyBracesStackCount--;
-        } else if (ch == OPENING_CURLY_BRACE) {
-          curlyBracesStackCount++;
-        }
-        if (curlyBracesStackCount >= 0) {
-          curVal.append(ch);
-        }
+      } else if (chars[current] == DOUBLE_QUOTES) {
+        current = tryParseQuotedString(current, chars, delimiter, values);
+      } else if (chars[current] == OPENING_CURLY_BRACE || chars[current] == OPENING_SQUARE_BRACKET) {
+        current = tryParseJsonObject(current, chars, delimiter, values);
       } else {
-        if (ch == DOUBLE_QUOTES) {
-          inQuotes = curlyBracesStackCount > 0 ? false : true;
-          if (startCollectChar) {
-            curVal.append(ch);
-          }
-        } else if (ch == OPENING_CURLY_BRACE) {
-          curlyBracesStackCount++;
-          curVal.append(ch);
-        } else if (ch == delimiter) {
-          addValueToList(headerValues, curVal);
-          curVal = new StringBuffer();
-          startCollectChar = false;
-        } else if (ch == '\r') {
-          continue;
-        } else if (ch == '\n') {
-          continue;
-        } else {
-          curVal.append(ch);
-        }
+        current = tryParseRawString(current, chars, delimiter, values);
       }
     }
-    addValueToList(headerValues, curVal);
-    return headerValues;
+    return values;
   }
 
-  private void addValueToList(List<String> headerValues, StringBuffer curVal) {
-    String value = curVal.toString();
-    if (isNotBlank(value)) {
-      headerValues.add(value);
+  private static int skipWhiteSpace(int start, char[] chars) {
+    int current = start;
+    while (current < chars.length && SPACE_CHARS.contains(chars[current])) {
+      current++;
     }
+    return current;
   }
 
+  private int tryParseQuotedString(int start, char[] chars, char delimiter, List<String> values) {
+    StringBuffer curVal = new StringBuffer();
+    // Skip initial quotes from being added to value
+    int current = start + 1;
+
+    // While unescaped quotes not found
+    while (current < chars.length && chars[current] != DOUBLE_QUOTES) {
+      if (chars[current] == '\\' && current < chars.length - 1 && chars[current + 1] == DOUBLE_QUOTES) {
+        // Found '\' followed by double quotes, skipping '\' character
+        current++;
+      }
+      curVal.append(chars[current]);
+      current++;
+    }
+
+    // Skip end quotes from being added to value
+    current++;
+    current = skipWhiteSpace(current, chars);
+
+    // End of string or delimiter found, adding value to result
+    if (current == chars.length || chars[current] == delimiter) {
+      values.add(curVal.toString());
+      return current + 1;
+    }
+
+    // Otherwise try parsing as Raw value
+    return tryParseRawString(start, chars, delimiter, values);
+  }
+
+  private int tryParseJsonObject(int start, char[] chars, char delimiter, List<String> values) {
+    StringBuffer curVal = new StringBuffer();
+    int current = start;
+    Stack<Character> stack = new Stack<>();
+    Character top;
+
+    do {
+      if (chars[current] == DOUBLE_QUOTES) {
+        // Consider everything until next quotes as a string with escaped quotes
+        do {
+          if (chars[current] == '\\') {
+            curVal.append(chars[current]);
+            current++;
+          }
+          if (current < chars.length) {
+            curVal.append(chars[current]);
+            current++;
+          }
+        } while (current < chars.length && chars[current] != DOUBLE_QUOTES);
+      } else if (chars[current] == CLOSING_CURLY_BRACE) {
+        top = stack.pop();
+        // If no opening curly brace in top of stack, unbalanced JSON object was found, try parsing as raw string
+        if (top != OPENING_CURLY_BRACE) {
+          return tryParseRawString(start, chars, delimiter, values);
+        }
+      } else if (chars[current] == CLOSING_SQUARED_BRACKET) {
+        top = stack.pop();
+        // If no opening square bracket in top of stack, unbalanced JSON array was found, try parsing as raw string
+        if (top != OPENING_SQUARE_BRACKET) {
+          return tryParseRawString(start, chars, delimiter, values);
+        }
+      } else if (chars[current] == OPENING_SQUARE_BRACKET || chars[current] == OPENING_CURLY_BRACE) {
+        stack.push(chars[current]);
+      }
+
+      if (current < chars.length) {
+        curVal.append(chars[current]);
+        current++;
+      }
+    } while (current < chars.length && !stack.isEmpty());
+
+    current = skipWhiteSpace(current, chars);
+
+    // End of string or delimiter found, adding value to result
+    if (current == chars.length || chars[current] == delimiter) {
+      values.add(curVal.toString());
+      return current + 1;
+    }
+
+    // Otherwise try parsing as Raw value
+    return tryParseRawString(start, chars, delimiter, values);
+  }
+
+  private int tryParseRawString(int start, char[] chars, char delimiter, List<String> values) {
+    int current = start;
+    current = skipWhiteSpace(current, chars);
+    int indexFirstNonWhitespace = current;
+    int indexLastNonWhitespace = current;
+    while (current < chars.length && chars[current] != delimiter) {
+      if (!SPACE_CHARS.contains(chars[current])) {
+        indexLastNonWhitespace = current;
+      }
+      current++;
+    }
+    // Trim string
+    values.add(new String(chars, indexFirstNonWhitespace, indexLastNonWhitespace - indexFirstNonWhitespace + 1));
+    return current + 1;
+  }
 }
