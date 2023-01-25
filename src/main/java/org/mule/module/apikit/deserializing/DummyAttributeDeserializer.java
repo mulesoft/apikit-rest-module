44456113b00f7306c1a224aa4304fa0011d96c3a
Damián Courtil <dcourtil@mulesoft.com>
2023-01-25 16:54:11 -0300

    W-11365512: Fix parsing of repeated array headers with quotes (#630) (#639)

--- src/main/java/org/mule/module/apikit/deserializing/DummyAttributeDeserializer.java
+++ src/main/java/org/mule/module/apikit/deserializing/DummyAttributeDeserializer.java
@@ -6,18 +6,48 @@
  */
 package org.mule.module.apikit.deserializing;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static java.util.Arrays.asList;
 
 /**
- * Dummy attribute deserializer that returns the same values that receives without modifying it.
- * Used for keeping backward compatibility with the default behaviour.
+ * Dummy attribute deserializer that returns the same values that receives.
+ * <p>
+ * If value is between quotes, it gets rid of enclosing quotes and unescapes inner quotes if any.
+ * If unbalanced quotes are find, the value is returned as it is.
+ * <p>
+ * Default deserializer when no delimiter strategy is set up.
  */
 public class DummyAttributeDeserializer extends BaseAttributeDeserializer {
 
   @Override
   public List<String> deserializeValue(String attributeValue) {
+    char[] chars = attributeValue.toCharArray();
+    if (chars[0] == DOUBLE_QUOTES && chars[chars.length - 1] == DOUBLE_QUOTES) {
+      return parseQuotedAttribute(attributeValue);
+    }
     return asList(attributeValue);
+  }
+
+  private List<String> parseQuotedAttribute(String attributeValue) {
+    char[] chars = attributeValue.toCharArray();
+    StringBuffer curVal = new StringBuffer();
+
+    List<String> values = new ArrayList<>();
+    for (int current = 1; current < chars.length - 1; current++) {
+      if (chars[current] == '\\') {
+        int next = current + 1;
+        if (next < chars.length - 1 && chars[next] == DOUBLE_QUOTES) {
+          current = next;
+        }
+      } else if (chars[current] == DOUBLE_QUOTES) {
+        // Unbalanced quotes found
+        return asList(attributeValue);
+      }
+      curVal.append(chars[current]);
+    }
+    values.add(curVal.toString());
+    return values;
   }
 }
