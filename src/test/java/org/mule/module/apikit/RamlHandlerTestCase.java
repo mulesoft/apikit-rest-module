c4fdbfed4dcea8a3fa5cefab67fb1e792eedd7f8
Damián Courtil <dcourtil@mulesoft.com>
2023-03-16 11:13:25 -0300

    Revert "W-11659429: Adopt AMF 5 (#628)"
    This reverts commit a3adebd8372fdc191834a79cb870a7816c487fff.

--- src/test/java/org/mule/module/apikit/RamlHandlerTestCase.java
+++ src/test/java/org/mule/module/apikit/RamlHandlerTestCase.java
@@ -121,7 +121,7 @@ public class RamlHandlerTestCase {
     assertTrue(ramlV2.contains("baseUri: " + apiServer));
 
     String ramlAmf = handler.getAMFModel();
-    assertTrue(ramlAmf.contains("baseUri: " + apiServer));
+    assertThat(ramlAmf, containsString("\"" + apiServer + "\""));
   }
 
   @Test
