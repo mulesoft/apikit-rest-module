f068b3160cb4af281f7d651bc1ee9f9febafce5c
ashutoshsharmasf <ashutoshsharma@salesforce.com>
2024-12-26 17:08:24 +0530

    compilation error fixed

--- src/test/java/org/mule/module/apikit/uri/TokenBaseDummyTest.java
+++ src/test/java/org/mule/module/apikit/uri/TokenBaseDummyTest.java
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
 package org.mule.module.apikit.uri;
 
 import org.junit.jupiter.api.Assertions;
@@ -16,6 +22,4 @@ class TokenBaseDummyTest {
         assertNotEquals(tokenBaseDummyA, tokenBaseDummyB);
         TokenBaseDummy.forTest("{test}");
     }
-
-
 }
